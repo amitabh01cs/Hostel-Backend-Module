2124dca49ed27c32e212997c741844587d8db917
amitabh01cs <146562758+amitabh01cs@users.noreply.github.com>
2025-09-04 18:02:34 +0530

    Update and rename UserActivityLogRepository to UserActivityLogRepository.java

--- src/main/java/com/example/testfrontendbackenddb/repository/UserActivityLogRepository.java
+++ src/main/java/com/example/testfrontendbackenddb/repository/UserActivityLogRepository.java
@@ -2,7 +2,9 @@ package com.example.testfrontendbackenddb.repository;
 
 import com.example.testfrontendbackenddb.model.UserActivityLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserActivityLogRepository extends JpaRepository<UserActivityLog, Long> {
-    // You can add custom query methods if needed
+    // No extra code needed
 }
