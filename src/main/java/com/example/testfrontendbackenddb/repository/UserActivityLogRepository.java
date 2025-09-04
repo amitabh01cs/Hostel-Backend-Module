7c6384e73f5877069a3b38cc3f46137fda108e70
amitabh01cs <146562758+amitabh01cs@users.noreply.github.com>
2025-09-04 17:27:57 +0530

    Create UserActivityLogRepository.java

---
+++ src/main/java/com/example/testfrontendbackenddb/repository/UserActivityLogRepository.java
@@ -0,0 +1,10 @@
+package com.example.testfrontendbackenddb.repository;
+
+  
+import com.yourpackage.entity.UserActivityLog;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserActivityLogRepository extends JpaRepository<UserActivityLog, Long> {
+}
