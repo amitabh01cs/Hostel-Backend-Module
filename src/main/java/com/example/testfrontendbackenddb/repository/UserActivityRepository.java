ec94fd3ce096a17bb1492d223d90ee664d9f06fb
amitabh01cs <146562758+amitabh01cs@users.noreply.github.com>
2025-09-14 12:24:33 +0530

    Update UserActivityRepository.java

--- src/main/java/com/example/testfrontendbackenddb/repository/UserActivityRepository.java
+++ src/main/java/com/example/testfrontendbackenddb/repository/UserActivityRepository.java
@@ -1,6 +1,6 @@
 package com.example.testfrontendbackenddb.repository;
 
-import com.example.testfrontendbackenddb.entity;
+import com.example.testfrontendbackenddb.entity.UserActivity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
